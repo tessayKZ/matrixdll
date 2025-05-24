927262bb3f4a90c8eb5dd35d5b8b2a98662655d0
Olzhas <tessay228@gmail.com>
2025-05-24 10:57:35 +0500

    improved

--- go.mod
+++ go.mod
@@ -7,20 +7,10 @@ require (
 	github.com/pion/rtp v1.8.15
 	github.com/pion/webrtc/v3 v3.3.5
 	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32
-	maunium.net/go/mautrix v0.24.0
 )
 
 require (
-	golang.org/x/mod v0.24.0 // indirect
-	golang.org/x/sync v0.14.0 // indirect
-	golang.org/x/tools v0.33.0 // indirect
-)
-
-require (
-	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pion/datachannel v1.5.8 // indirect
 	github.com/pion/dtls/v2 v2.2.12 // indirect
 	github.com/pion/ice/v2 v2.3.36 // indirect
@@ -36,19 +26,10 @@ require (
 	github.com/pion/transport/v2 v2.2.10 // indirect
 	github.com/pion/turn/v2 v2.1.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
-	github.com/tidwall/gjson v1.18.0 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.1 // indirect
-	github.com/tidwall/sjson v1.2.5 // indirect
 	github.com/wlynxg/anet v0.0.3 // indirect
-	go.mau.fi/util v0.8.7 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
-	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
-	golang.org/x/mobile v0.0.0-20250520180527-a1d90793fc63
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- matrixDLL.go
+++ matrixDLL.go
@@ -1,21 +1,20 @@
 package matrixdll
 
 import (
-	"context"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"time"
 
-	 _ "golang.org/x/mobile/bind"
 	"github.com/google/uuid"
 	"github.com/pion/rtp/codecs"
 	"github.com/pion/webrtc/v3"
 	"github.com/pion/webrtc/v3/pkg/media"
 	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
 	"layeh.com/gopus"
-	"maunium.net/go/mautrix"
-	"maunium.net/go/mautrix/event"
-	"maunium.net/go/mautrix/id"
 )
 
 const (
@@ -26,49 +25,71 @@ const (
 	syncRetryGap = time.Second
 )
 
+// HTTP helper
+func postJSON(url string, payload interface{}, out interface{}) error {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 300 {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("HTTP %d: %s", resp.StatusCode, string(body))
+	}
+	return json.NewDecoder(resp.Body).Decode(out)
+}
+
+// Matrix API paths
+const (
+	loginPath     = "/_matrix/client/r0/login"
+	sendEventPath = "/_matrix/client/r0/rooms/%s/send/%s/%s"
+)
+
+type loginReq struct {
+	Type       string                 `json:"type"`
+	Identifier map[string]interface{} `json:"identifier"`
+	Password   string                 `json:"password"`
+}
+type loginResp struct {
+	UserID      string `json:"user_id"`
+	AccessToken string `json:"access_token"`
+}
+
+// Client for Matrix call
 type Client struct {
-	homeserver    string
-	username      string
-	password      string
-	roomID        string
-	mautrixClient *mautrix.Client
-	pc            *webrtc.PeerConnection
+	homeserver  string
+	accessToken string
+	userID      string
+
+	roomID string
+	pc     *webrtc.PeerConnection
 
 	currentCallID string
 	myPartyID     string
-	myUserID      id.UserID
 
 	dataCh   chan []int16
 	decodeCh chan []int16
 }
 
+// NewClient logs in and prepares WebRTC PeerConnection
 func NewClient(homeserver, username, password, roomID string) (*Client, error) {
-	c := &Client{
-		homeserver: homeserver,
-		username:   username,
-		password:   password,
-		roomID:     roomID,
-		dataCh:     make(chan []int16, 50),
-		decodeCh:   make(chan []int16, 50),
-	}
-
-	mcl, err := mautrix.NewClient(c.homeserver, "", "")
-	if err != nil {
-		return nil, fmt.Errorf("mautrix NewClient: %w", err)
-	}
-	resp, err := mcl.Login(context.Background(), &mautrix.ReqLogin{
+	// Login
+	url := homeserver + loginPath
+	req := loginReq{
 		Type:       "m.login.password",
-		Identifier: mautrix.UserIdentifier{Type: "m.id.user", User: c.username},
-		Password:   c.password,
-	})
-	if err != nil {
+		Identifier: map[string]interface{}{"type": "m.id.user", "user": username},
+		Password:   password,
+	}
+	var resp loginResp
+	if err := postJSON(url, req, &resp); err != nil {
 		return nil, fmt.Errorf("login error: %w", err)
 	}
-	mcl.SetCredentials(resp.UserID, resp.AccessToken)
-	c.mautrixClient = mcl
-	c.myUserID = resp.UserID
-	c.myPartyID = uuid.NewString()
 
+	// Prepare PeerConnection
 	conf := webrtc.Configuration{
 		ICETransportPolicy: webrtc.ICETransportPolicyAll,
 		ICEServers: []webrtc.ICEServer{
@@ -81,53 +102,48 @@ func NewClient(homeserver, username, password, roomID string) (*Client, error) {
 		return nil, fmt.Errorf("NewPeerConnection: %w", err)
 	}
 
+	c := &Client{
+		homeserver:    homeserver,
+		accessToken:   resp.AccessToken,
+		userID:        resp.UserID,
+		roomID:        roomID,
+		pc:            pc,
+		myPartyID:     uuid.NewString(),
+		dataCh:        make(chan []int16, 50),
+		decodeCh:      make(chan []int16, 50),
+	}
+
+	// ICE candidate handler
 	pc.OnICECandidate(func(cand *webrtc.ICECandidate) {
 		if cand == nil {
 			return
 		}
 		ice := cand.ToJSON()
 		payload := map[string]interface{}{
-			"call_id":  c.currentCallID,
-			"party_id": c.myPartyID,
-			"version":  "1",
-			"candidates": []interface{}{
-				map[string]interface{}{
-					"candidate":     ice.Candidate,
-					"sdpMid":        ice.SDPMid,
-					"sdpMLineIndex": ice.SDPMLineIndex,
-				},
-			},
+			"call_id":   c.currentCallID,
+			"party_id":  c.myPartyID,
+			"version":   "1",
+			"candidates": []interface{}{map[string]interface{}{
+				"candidate":     ice.Candidate,
+				"sdpMid":        ice.SDPMid,
+				"sdpMLineIndex": ice.SDPMLineIndex,
+			}},
 		}
-		if _, err := c.mautrixClient.SendMessageEvent(
-			context.Background(),
-			id.RoomID(c.roomID),
-			event.CallCandidates,
-			payload,
-		); err != nil {
-			log.Println("Send ICE candidate error:", err)
+		txn := uuid.NewString()
+		url := fmt.Sprintf(c.homeserver+sendEventPath, c.roomID, "m.call.candidates", txn)
+		if err := postJSON(url, payload, &map[string]interface{}{}); err != nil {
+			log.Println("Send candidates error:", err)
 		}
 	})
 
-	// Log state changes
-	pc.OnConnectionStateChange(func(state webrtc.PeerConnectionState) {
-		log.Println("PeerConnection state:", state)
-	})
-	pc.OnICEConnectionStateChange(func(state webrtc.ICEConnectionState) {
-		log.Println("ICE connection state:", state)
-	})
-
-	// Incoming audio handler
+	// Track handler
 	pc.OnTrack(func(track *webrtc.TrackRemote, _ *webrtc.RTPReceiver) {
 		if track.Kind() != webrtc.RTPCodecTypeAudio {
 			return
 		}
 		go func() {
 			sb := samplebuilder.New(10, &codecs.OpusPacket{}, track.Codec().ClockRate)
-			dec, err := gopus.NewDecoder(sampleRate, channels)
-			if err != nil {
-				log.Println("Decoder init error:", err)
-				return
-			}
+			dec, _ := gopus.NewDecoder(sampleRate, channels)
 			for {
 				pkt, _, err := track.ReadRTP()
 				if err != nil {
@@ -135,11 +151,7 @@ func NewClient(homeserver, username, password, roomID string) (*Client, error) {
 				}
 				sb.Push(pkt)
 				for s := sb.Pop(); s != nil; s = sb.Pop() {
-					pcm, err := dec.Decode(s.Data, frameSize, false)
-					if err != nil {
-						log.Println("Decode error:", err)
-						break
-					}
+					pcm, _ := dec.Decode(s.Data, frameSize, false)
 					select {
 					case c.decodeCh <- pcm:
 					default:
@@ -149,91 +161,50 @@ func NewClient(homeserver, username, password, roomID string) (*Client, error) {
 		}()
 	})
 
-	c.pc = pc
 	return c, nil
 }
 
+// StartCall creates offer and sends invite
 func (c *Client) StartCall() error {
-	enc, err := gopus.NewEncoder(sampleRate, channels, gopus.Voip)
-	if err != nil {
-		return fmt.Errorf("gopus encoder: %w", err)
-	}
-	sendTrack, err := webrtc.NewTrackLocalStaticSample(
+	// Add audio track
+	enc, _ := gopus.NewEncoder(sampleRate, channels, gopus.Voip)
+	sendTrack, _ := webrtc.NewTrackLocalStaticSample(
 		webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeOpus, ClockRate: sampleRate, Channels: channels},
 		"matrix-send", "audio",
 	)
-	if err != nil {
-		return fmt.Errorf("create send track: %w", err)
-	}
-	if _, err := c.pc.AddTrack(sendTrack); err != nil {
-		return fmt.Errorf("add send track: %w", err)
-	}
+	c.pc.AddTrack(sendTrack)
+
+	// Connection state
 	c.pc.OnConnectionStateChange(func(state webrtc.PeerConnectionState) {
 		if state == webrtc.PeerConnectionStateConnected {
 			go func() {
 				for pcm := range c.dataCh {
-					pkt, err := enc.Encode(pcm, frameSize, opusBufSize)
-					if err != nil {
-						log.Println("encode error:", err)
-						continue
-					}
-					if err := sendTrack.WriteSample(media.Sample{Data: pkt, Duration: 20 * time.Millisecond}); err != nil {
-						log.Println("WriteSample error:", err)
-					}
+					data, _ := enc.Encode(pcm, frameSize, opusBufSize)
+					sendTrack.WriteSample(media.Sample{Data: data, Duration: 20 * time.Millisecond})
 				}
 			}()
 		}
 	})
 
-	go func() {
-		for {
-			if err := c.mautrixClient.Sync(); err != nil {
-				log.Println("Sync error:", err)
-				time.Sleep(syncRetryGap)
-			}
-		}
-	}()
+	// Sync placeholder: implement your own sync if needed
 
+	// Create offer
+	off, _ := c.pc.CreateOffer(nil)
+	c.pc.SetLocalDescription(off)
+	<-webrtc.GatheringCompletePromise(c.pc)
 	c.currentCallID = fmt.Sprintf("call-%d", time.Now().Unix())
-	sdp, err := BuildOfferSDP(c.pc)
-	if err != nil {
-		return fmt.Errorf("BuildOfferSDP error: %w", err)
-	}
-	invite := map[string]interface{}{
-		"call_id":  c.currentCallID,
-		"lifetime": 60000,
-		"offer":    map[string]interface{}{"type": "offer", "sdp": sdp},
-		"version":  "1",
-		"party_id": c.myPartyID,
-	}
-	if _, err := c.mautrixClient.SendMessageEvent(
-		context.Background(),
-		id.RoomID(c.roomID),
-		event.CallInvite,
-		invite,
-	); err != nil {
-		return fmt.Errorf("send invite: %w", err)
+	invite := map[string]interface{}{ "call_id": c.currentCallID, "party_id": c.myPartyID, "lifetime": 60000, "offer": map[string]interface{}{"type": "offer", "sdp": off.SDP}, "version": "1" }
+	txn := uuid.NewString()
+	url := fmt.Sprintf(c.homeserver+sendEventPath, c.roomID, "m.call.invite", txn)
+	if err := postJSON(url, invite, &map[string]interface{}{}); err != nil {
+		return err
 	}
 	return nil
 }
 
-func BuildOfferSDP(pc *webrtc.PeerConnection) (string, error) {
-	off, err := pc.CreateOffer(nil)
-	if err != nil {
-		return "", fmt.Errorf("CreateOffer error: %w", err)
-	}
-	if err := pc.SetLocalDescription(off); err != nil {
-		return "", fmt.Errorf("SetLocalDescription error: %w", err)
-	}
-	<-webrtc.GatheringCompletePromise(pc)
-	return off.SDP, nil
-}
-
-func (c *Client) SendAudio(data []byte) error {
-	if c.dataCh == nil {
-		return fmt.Errorf("client not initialized")
-	}
-	n := len(data) / 2
+// SendAudio queues PCM bytes for sending
+func (c *Client) SendAudio(data []byte) {
+	n := len(data)/2
 	samples := make([]int16, n)
 	for i := 0; i < n; i++ {
 		samples[i] = int16(data[2*i]) | int16(data[2*i+1])<<8
@@ -242,18 +213,15 @@ func (c *Client) SendAudio(data []byte) error {
 	case c.dataCh <- samples:
 	default:
 	}
-	return nil
 }
 
-func (c *Client) ReceiveAudio() ([]byte, error) {
-	if c.decodeCh == nil {
-		return nil, fmt.Errorf("client not initialized")
-	}
+// ReceiveAudio returns next PCM frame
+func (c *Client) ReceiveAudio() []byte {
 	pcm := <-c.decodeCh
 	out := make([]byte, len(pcm)*2)
 	for i, v := range pcm {
 		out[2*i] = byte(v)
 		out[2*i+1] = byte(v >> 8)
 	}
-	return out, nil
+	return out
 }
